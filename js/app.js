e710d7dfb4369065647fd817454e56e12718b0cd
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-09-14 19:29:55 -0700

    add array of initial coffee shop locations.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// Initial Variable
+var initialLocations = [
+  {title: "SightGlass Coffee", location: {lat: 37.776981, lng: -122.408586}},
+  {title: "Blue Bottle Coffee", location: {lat: 37.776392, lng: -122.423337}},
+  {title: "Ritual Coffee", location: {lat: 37.776384, lng: -122.424173}},
+  {title: "Four Barrel Coffee", location: {lat: 37.767010, lng: -122.421973}},
+  {title: "Réveille Coffee Co.", location: {lat: 37.761032, lng: -122.434438}}
+];
+
 
 // Basic ViewModel
 var ViewModel = function(){
